77478e69d9d618afcd1fb1835a2e7b9e2a38f15f
Sadid369 <sadid.jones@gmail.com>
2022-04-17 00:41:24 +0600

    ok

--- main.py
+++ main.py
@@ -15,4 +15,8 @@ for question in question_data:
 
 quiz = QuizBrain(question_bank)
 
-quiz.next_question()
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("you've completed the quiz")
+print(f"your final score was: {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -2,7 +2,21 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score= 0
+    def still_has_question(self):
+        return self.question_number<len(self.question_list)
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number+=1
-        input(f"Q.{self.question_number} : {self.question_list[self.question_number].text} (True/False): ")
+        user_answer= input(f"Q.{self.question_number} : {self.question_list[self.question_number].text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score+=1
+            print("you got it right!")
+        else:
+            # self.score-=1
+            print("that's wrong")
+        print(f"The Correct answer was: {correct_answer}")
+        print(f"Your current score is : {self.score}/{self.question_number}")
+        print("\n")
